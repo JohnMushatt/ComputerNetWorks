5489c7cd5b390c20787723590bf56644e9e45782
John Mushatt <john.mushatt@gmail.com>
2019-06-21 14:58:41 -0400

    Added initial client and username for #2

--- ChatProgram/Client/client.c
+++ ChatProgram/Client/client.c
@@ -5,4 +5,56 @@
  *      Author: john
  */
 
+// Client side C/C++ program to demonstrate Socket programming
 
+#define PORT 8080
+#include "client.h"
+char* fixInput(char *input) {
+	int len = strlen(input);
+	char *newString = malloc(sizeof(char) * (len));
+	strncpy(newString, input, len - 2);
+
+}
+char *connectionMessage(char *username) {
+
+}
+int main(int argc, char const *argv[]) {
+	int sock = 0;
+	long valread;
+	struct sockaddr_in serv_addr;
+	char buffer[1024] = { 0 };
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("\n Socket creation error \n");
+		return -1;
+	}
+
+	memset(&serv_addr, '0', sizeof(serv_addr));
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+
+	// Convert IPv4 and IPv6 addresses from text to binary form
+	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+		printf("\nInvalid address/ Address not supported \n");
+		return -1;
+	}
+
+	if (connect(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0) {
+		printf("\nConnection Failed \n");
+		return -1;
+	} else {
+		printf(
+				"Please enter username to connect as (Limited to 20 characters):");
+		char *username = malloc(sizeof(char) * 20);
+		fgets(username, 20, stdin);
+		username = fixInput(username);
+		printf("Your username: %s!\n", username);
+		char *message = "USER:"
+		send(sock, hello, strlen(hello), 0);
+		printf("Hello message sent\n");
+		valread = read(sock, buffer, 1024);
+		printf("%s\n", buffer);
+	}
+
+	return 0;
+}
--- ChatProgram/Client/client.h
+++ ChatProgram/Client/client.h
@@ -9,5 +9,14 @@
 #define CLIENT_CLIENT_H_
 
 
-
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
 #endif /* CLIENT_CLIENT_H_ */
+
+char *fixInput(char *input);
+
