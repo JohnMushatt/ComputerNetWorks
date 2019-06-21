69d4486af4abcb9fa6f5fd0a09cd6687045a15f4
John Mushatt <john.mushatt@gmail.com>
2019-06-21 04:58:22 -0400

    Fixed g++ error, and implemented initial server model for #1 and #3

--- ChatProgram/Server/server.h
+++ ChatProgram/Server/server.h
@@ -8,6 +8,11 @@
 #ifndef SERVER_SERVER_H_
 #define SERVER_SERVER_H_
 
-
-
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#define PORT 8080
 #endif /* SERVER_SERVER_H_ */
